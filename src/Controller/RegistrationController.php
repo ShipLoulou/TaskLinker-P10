1b1c57011544b2f3a2a9e18ff0deed7b58f88d46
Loulou <shiploulou.dev@gmail.com>
2025-01-29 10:51:23 +0100

    création de la page login et du système de connexion

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -38,7 +38,7 @@ class RegistrationController extends AbstractController
 
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_projects');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
